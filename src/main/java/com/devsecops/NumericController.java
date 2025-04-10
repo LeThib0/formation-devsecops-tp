6141024ff4aed63f5f896eba9e2b1fd10014c8d7
LeThib0 <thibaut.amourous@gmail.com>
2025-04-10 15:33:01 +0200

    modifJenkins

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -34,9 +34,9 @@ public class NumericController {
 		public String compareToFifty(@PathVariable int value) {
 			String message = "Could not determine comparison";
 			if (value > 50) {
-				message = "Greater than 50";
+				message = "Plus grand que 50";
 			} else {
-				message = "Smaller than or equal to 50";
+				message = "Plus petit que 50";
 			}
 			return message;
 		}
